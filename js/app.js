06b845ff20a1221efae5dc4840f4d7dc136742ed
bruceabrigo <bruceabrigo@outlook.com>
2023-01-20 08:54:35 -0600

    Characters can now show and close bios

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// functions were already called in html
+const showBio1 = () => {
+  status1.innerText = ''
+  bio1.innerText = 'Finn (the Human) Mertens, son to Martin Mertens and Minverva Campbell, ends up on the mysterious land of Ooo. To which he would fall ownership to a family of dogs.'
+  closeContent1.style.display = 'block'
+}
+
+const showStatus1 = () => {
+  bio1.innerText = ''
+  status1.innerText = 'Crying about flame princess'
+  closeContent1.style.display = 'block'
+}
+
+const closeCard1 = () => {
+  status1.innerText = ''
+  bio1.innerText = ''
+  closeContent1.style.display = 'none'
+}
+
+const showBio2 = () => {
+  status2.innerText = ''
+  bio2.innerText = 'Practically grew up and raised with his best bud Finn the Human. Jake the (mutant) dog has stretchy alien powers that come into play when adventure time calls'
+  closeContent2.style.display = 'block'
+}
+
+const showStatus2 = () => {
+  status2.innerText = 'Playing video games with BMO'
+  bio2.innerText = ''
+  closeContent2.style.display = 'block'
+}
+
+const closeCard2 = () => {
+  status2.innerText = ''
+  bio2.innerText = ''
+  closeContent2.style.display = 'none'
+}
+
+const showBio3 = () => {
+  status3.innerText = ''
+  bio3.innerText = 'Girl turned vampire with no desire to kill. UNLESS its protecting her friends. Though she may nor seem intimadating. She can turn invisible, has self healing, and can morph into the most terrifying thing youve ever seen'
+  closeContent3.style.display = 'block'
+}
+
+const showStatus3 = () => {
+  status3.innerText = 'Drinking red stuff and playing guitar'
+  bio3.innerText = ''
+  closeContent3.style.display = 'block'
+}
+
+const closeCard3 = () => {
+  status3.innerText = ''
+  bio3.innerText = ''
+  closeContent3.style.display = 'none'
+}
